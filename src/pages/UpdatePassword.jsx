7c1af5ef1c15825b335342fd76dc4b8f6c388d96
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2025-05-05 11:49:40 +0530

    Update UpdatePassword.jsx

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -1,74 +1,98 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import { useDispatch } from 'react-redux';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { resetPassword } from '../services/operations/authApi';
-
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { resetPassword } from "../services/operations/authApi";
 
 export const UpdatePassword = () => {
-const [data, setData] = useState({
-    password:"",
-    confirmPassword:""
-})
-const dispatch = useDispatch();
-const navigate = useNavigate();
-const [watchPassword, setWatchPassword] = useState(false)
-const { token } = useParams();
-console.log("Token Passed from frontend ->",token)
-const {password, confirmPassword} = data
+  const [data, setData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [watchPassword, setWatchPassword] = useState(false);
+  const { token } = useParams();
+  console.log("Token Passed from frontend ->", token);
+  const { password, confirmPassword } = data;
 
-
-
-const handleChange = (event) =>{
-    const {name, value} = event.target;
-    setData((prevData)=>({
-        ...prevData, [name]:value
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
     }));
-    
-}
+  };
 
-if(!token){
+  if (!token) {
     toast.error("token is Invalid");
-    return
-}
+    return;
+  }
 
-const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if(!password || !confirmPassword){
-        toast.error("Fill all the details correctly!");
-        return;
+    if (!password || !confirmPassword) {
+      toast.error("Fill all the details correctly!");
+      return;
     }
 
     if (password !== confirmPassword) {
-        setWatchPassword(true);
-        toast.error("Passwords do not match.");
-        return;
+      setWatchPassword(true);
+      toast.error("Passwords do not match.");
+      return;
     }
-    dispatch(resetPassword({password, confirmPassword, token, navigate}))
-}
-
+    dispatch(resetPassword({ password, confirmPassword, token, navigate }));
+    setData({
+      password: "",
+      confirmPassword: "",
+    });
+  };
 
   return (
-    <div>
-        <div>
-            <div>Choose  new password</div>
-            <div>Almost done. Enter your new password and youre all set.</div>
+    <div className="flex flex-col text-richblack-50 h-screen items-center justify-center bg-richblack-900 -mt-20">
+      <div className="border-richblack-100 border-[1px] p-3 rounded-xl bg-richblack-800 flex flex-col gap-3">
+        <div className="flex flex-col items-start">
+          <div className="text-[36px] font-semibold">Choose new password</div>
+          <div className="text-sm text-richblack-300 ">
+            Almost done. Enter your new password and youre all set.
+          </div>
         </div>
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="password">New Password: <span>*</span></label>
-                    <input type="password" name='password' value={data.password} id='password' className='' onChange={handleChange}/>
-                </div>
-                <div>
-                    <label htmlFor="confirmPassword">Confirm New Password: <span>*</span></label>
-                    <input type="password" name='confirmPassword' value={data.confirmPassword} id='confirmPassword' className='' onChange={handleChange}/>
-                    <div className={`${watchPassword === true?`flex`:`hidden`}`}>*Confirm password is not matched with password</div>
-                </div>
-                <button type="submit">Reset Password</button>
-            </form>
+          <form onSubmit={handleSubmit} className="flex flex-col items-start gap-3">
+            <div className="flex flex-col">
+              <label htmlFor="password">
+                New Password: <span className="text-pink-200">*</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={data.password}
+                id="password"
+                className="w-[20rem] bg-richblack-900 p-2 rounded-xl text-richblack-50 outline-none"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="confirmPassword">
+                Confirm New Password: <span>*</span>
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={data.confirmPassword}
+                id="confirmPassword"
+                className="w-[20rem] bg-richblack-900 p-2 rounded-xl text-richblack-50 outline-none"
+                onChange={handleChange}
+              />
+              <div className={`${watchPassword === true ? `flex` : `hidden`}`}>
+                *Confirm password is not matched with password
+              </div>
+            </div>
+            <button type="submit">Reset Password</button>
+          </form>
         </div>
         <Link to="/login">Back to Login</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
