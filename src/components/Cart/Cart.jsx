0543520d080a656348de849418c4ba9efe8f72eb
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2025-06-06 10:01:58 +0530

    Cart Section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,7 +6,7 @@ import { RenderTotalAmount } from './RenderTotalAmount';
 export const Cart = () => {
   const {totalItems} = useSelector((state) => state.cart)
   return (
-    <div className='bg-richblack-900 text-richblack-50 h-screen'>
+    <div className='bg-richblack-900 text-richblack-50 '>
       <div>My Cart</div>
       <div>Total {totalItems} Courses in the Cart</div>
       <div>
--- src/components/Cart/RenderCartCourses.jsx
+++ src/components/Cart/RenderCartCourses.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import courseImage from '../../assets/Images/Instructor.png'
+import ReactStars from "react-rating-stars-component";
 
 export const RenderCartCourses = () => {
   return (
-    <div>RenderCartCourses</div>
+    <div>
+      <div>
+        <img src={courseImage} alt="" />
+      </div>
+      <div>
+        <div>The complete python from zero to hero</div>
+        <div>Instructor name</div>
+        <div>
+          <ReactStars
+            count={5}
+            size={24}
+            value={4.5}
+            edit={false}
+            isHalf={true}
+            activeColor="#ffd700"
+          />  
+        </div>
+        <div>
+          <div>Total Courses</div>
+          <div>Lesson</div>
+          <div>Beginner</div>
+        </div>
+      </div>
+      <div>
+        <button>Remove</button>
+        <div>Rs.4000/-</div>
+        <div></div>
+      </div>
+    </div>
   )
 }
